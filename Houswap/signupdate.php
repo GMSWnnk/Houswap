b386a8d85c42f462bfb7bfec26a50e2f86348cc7
jswlt <jswlt@LAPTOP-II2HCG5R>
2020-02-12 17:24:23 +0900

    signupdate.php update

--- Houswap/signupdate.php
+++ Houswap/signupdate.php
@@ -2,39 +2,44 @@
 
 #----------sign up source--------------
 
-#check for user presence
+#check for user Exist
 $check_sql = "SELECT * FROM customer WHERE c_id='$c_id'";
 
-#id 존재여부를 DB를 통해 체크
-$check_num = mysqli_query($conn, $check_sql);
+#Exist ID check from DB
+$check_num =mysqli_query($conn, $check_sql);
 if(mysqli_num_rows($check_num)>0)
 {
     echo "존재하는 아이디입니다.";
     exit;
 }
 
-#Signup.php에서 form값 받기
-$c_id= trim(_POST['c_id']);
-$c_password= trim(_POST['c_password']);
-$c_name= trim(_POST['c_name']);
-$c_sex= trim(_POST['c_sex']);
-$c_email= trim(_POST['c_email']);
-$c_phonenum= trim(_POST['c_phonenum']);
-$c_adress= trim(_POST['c_adress']);
+#get form data from signup.php 
+$cs_id= trim($_POST['cs_id']);
+$cs_password= trim($_POST['cs_password']);
+$cs_name= trim($_POST['cs_name']);
+$cs_sex= trim($_POST['cs_sex']);
+$cs_email= trim($_POST['cs_email']);
+$cs_phonenum= trim($_POST['cs_phonenum']);
+$cs_adress= trim($_POST['cs_adress']);
 
-#Empty Error Message
-if(!$c_id){ echo "아이디를 입력해주세요.";}
-else if($c_id>5||$c_id<13){ echo"아이디는 5~12자 이내여야합니다.";}
-if(!$c_password){ echo "비밀번호를 입력해주세요.";}
-else if(!$c_name){ echo "이름을 입력해주세요.";}
-else if(!$c_sex){ echo "성별을 선택해주세요.";}
-else if(!$c_email){ echo "이메일을 입력해주세요.";}
-else if(!$c_phonenum){ echo "휴대폰번호를 입력해주세요.";}
-else if(!$c_adress){ echo "주소를 입력해주세요.";}
 
 #insert query
 $insert_q="INSERT INTO customer 
-           SET c_id='$c_id',
-               c_password='$c_password'";   
+           SET c_id='$cs_id',
+               c_password='$cs_password',   
+               c_name='$cs_name',
+               c_sex ='$cs_sex',
+               c_email ='$cs_mail',
+               c_phonenum='$cs_phonenum',
+               c_adress='$cs_adress'";
 
- ?>
+$insert_d=mysqli_query($conn,$insert_q);
+
+echo"<script>alert('회원가입이 완료되었습니다.')</script>;";
+echo"<script>location.replace('login.php')</script>";
+
+
+mysqli_close($conn);
+?>
+
+
