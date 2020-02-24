b8613d8e5ba16ab21e421558348da966aaac5c6f
jswlt <jswlt@LAPTOP-II2HCG5R>
2020-02-25 00:05:18 +0900

    0225 jisu

--- Houswap/mainview.php
+++ Houswap/mainview.php
@@ -9,35 +9,45 @@
 	</head>
 
 <body> 
+		
 		<?php if(!isset($_SESSION['ss_id'])){ ?>
-		<h5><a href="./login.php">로그인</a>
-		<a class=mypage href="./login.php">마이페이지</a></h5>		
+		<a href="./login.php">로그인</a>
+		<a class=mypage href="./login.php">마이페이지</a>
 		<?php 
 		}
 		
 		else{?>
-		<h5><a href="./logout.php">로그아웃</a>
-		<a class=mypage href="./mypage.php">마이페이지</a></h5>	  
+		<a href="./logout.php">로그아웃</a>
+		<a class=mypage href="./mypage.php">마이페이지</a>
 		<?php }?>
 		
+		
 	<!-- mainpage name, search -->
-	<h1><a class="title" href='./mainview.php'>Houswap</a> 
+	<div class=maintitle>
+		<a class="title" href='./mainview.php'>Houswap</a> 
 		<input type="text" placeholder="search">
 		<button>검색</button>
-	</h1>
+	</div>	
+
 
 	<div id="menu">
-		<div id="menufont">
-		<a class=menu>메뉴1</a> 
-		<a class=menu>메뉴2</a>
-		<a class=menu>메뉴3</a>
-		<a class=menu>메뉴4</a>
+		<div id=menuclass>
+		<table id=menutable>
+			<tr><td><a class=menu>메뉴1</a></td>
+			<td><a class=menu>메뉴2</a></td>
+			<td><a class=menu>메뉴3</a></td>
+			<td><a class=menu>메뉴4</a></td>
+			<td><a class=menu>메뉴5</a></td></tr>
+		</table>
 		</div>
 	</div>
 
+<div class="newtable_font">
 	<h4>최근 올라온 상품</h4>
-	<hr>
-	
+	<hr align="left">
+</div>
+		
+<div class="newtable">
 		<table>
 		<tr>
 			<td>
@@ -56,7 +66,6 @@
 			<?php echo "<img src='images/1111.jpg'width=100 height=100>"?>
 			</td>
 		</tr>
-		
 		<tr align=center>
 			<td>1</td>
 			<td>2</td>
@@ -65,6 +74,41 @@
 			<td>5</td>
 		</tr>
 		</table>
+</div>
+		
+<div class="newtable_font">
+	<h4>마감 임박 상품</h4>
+	<hr align="left">
+</div>
+
+<div class="newtable">
+		<table>
+		<tr>
+			<td>
+			<?php echo "<img src='images/1111.jpg'width=100 height=100>"?>
+			</td>
+			<td>
+			<?php echo "<img src='images/1111.jpg'width=100 height=100>"?>
+			</td>
+			<td>
+			<?php echo "<img src='images/1111.jpg'width=100 height=100>"?>
+			</td>
+			<td>
+			<?php echo "<img src='images/1111.jpg'width=100 height=100>"?>
+			</td>
+			<td>
+			<?php echo "<img src='images/1111.jpg'width=100 height=100>"?>
+			</td>
+		</tr>
+		<tr align=center>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+		</tr>
+		</table>
+</div>
 
 
 </body>
