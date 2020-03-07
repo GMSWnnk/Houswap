e8c0fe58f5c85dc7a098eff8bca3a5aec8ef734c
jswlt <jswlt@LAPTOP-II2HCG5R>
2020-03-07 19:15:17 +0900

    20.03.07 mypage upload -- jisu

--- Houswap/mainview.php
+++ Houswap/mainview.php
@@ -4,7 +4,7 @@
 
 <html>
 	<head>
-		<title>하우스왑</title>
+		<title>하우스왑 - 동네 애물단지 물물교환</title>
 		<link href="./mainpage.css" rel="stylesheet" type="text/css">
 	</head>
 
@@ -25,100 +25,34 @@
 		
 		
 	<!-- mainpage name, search -->
-	<div class=maintitle>
+	<div class=maintitle align="center">
 		<a class="title" href='./mainview.php'>Houswap</a> 
-		<input type="text" placeholder="search">
-		<button>검색</button>
-	</div>	
-
-	<div id="menu">
-		<div id="menuclass">
-		<table id="menutable">
-			<tr><td><a class=menu>메뉴1</a></td>
-			<td><a class=menu>메뉴2</a></td>
-			<td><a class=menu>메뉴3</a></td>
-			<td><a class=menu>메뉴4</a></td>
-			<td><a class=menu>메뉴5</a></td></tr>
-		</table>
-		</div>
 	</div>
+	<input type="text" placeholder="search"> <button>검색</button>
 </div>
 
-<div class=row>	
-	<div class="newtable">
-		<h4>최근 올라온 상품</h4>
-		<hr align="left">
-		<table class="photo">
-		<tr>
-			<td>
-			<?php echo "<img src='images/joongbu.png'width=150px height=150px>"?>
-			</td>
-			<td>
-			<?php echo "<img src='images/joongbu.png'width=150px height=150px>"?>
-			</td>
-			<td>
-			<?php echo "<img src='images/joongbu.png'width=150px height=150px>"?>
-			</td>
-		</tr>
-		<tr align=center>
-			<td>게시글1</td>
-			<td>게시글2</td>
-			<td>게시글3</td>
-		</tr>
-		
-		<tr>
-			<td>
-			<?php echo "<img src='images/joongbu.png'width=150px height=150px>"?>
-			</td>
-			<td>
-			<?php echo "<img src='images/joongbu.png'1111.jpg'width=150px height=150px>"?>
-			</td>
-			<td>
-			<?php echo "<img src='images/joongbu.png'width=150px height=150px>"?>
-			</td>
-		</tr>
-		<tr align=center>
-			<td>게시글4</td>
-			<td>게시글5</td>
-			<td>게시글6</td>
-		
-
-		</tr>
-		</table>
-	</div>
+	<div class="menu" align="center">
+	<div class="textline"><b id="new">최근 올라온 상품</b><a href="./morenewpage.php">더보기</a></div>
+<hr>
+	
+<div>
+	<div class=newtable></div>
+	<div class=newtable></div>
+	<div class=newtable></div>
+	<div class=newtable></div>
 </div>
 
-<div class="newtable">
-	<h4>마감 임박 상품</h4>
-	<hr align="left">	
-		<table class="photo">
-		<tr>
-			<td>
-			<?php echo "<img src='images/1111.jpg'width=100px height=100px>"?>
-			</td>
-			<td>
-			<?php echo "<img src='images/1111.jpg'width=100px height=100px>"?>
-			</td>
-			<td>
-			<?php echo "<img src='images/1111.jpg'width=100px height=100px>"?>
-			</td>
-			<td>
-			<?php echo "<img src='images/1111.jpg'width=100px height=100px>"?>
-			</td>
-			<td>
-			<?php echo "<img src='images/1111.jpg'width=100px height=100px>"?>
-			</td>
-		</tr>
-		<tr align=center>
-			<td>1</td>
-			<td>2</td>
-			<td>3</td>
-			<td>4</td>
-			<td>5</td>
-		</tr>
-		</table>
-</div>
 
+<div class="textline"><b id="deadline">마감 임박 상품</b><a href="./morenewpage.php">더보기</a></div>
+	<hr>
+	
+<div>
+	<div class=newtable></div>
+	<div class=newtable></div>
+	<div class=newtable></div>
+	<div class=newtable></div>
+</div>
+</div>
 
 </body>
 </html>
--- Houswap/mypage.php
+++ Houswap/mypage.php
@@ -14,23 +14,64 @@
 		<?php if(!isset($_SESSION['ss_id'])){ ?>
 		<a href="./login.php">로그인</a>
 		<a class=mypage href="./login.php">마이페이지</a>
-		<?php 
-		}
-		
+        <?php } 
+        
 		else{?>
 		<a href="./logout.php">로그아웃</a>
-		<a class=mypage href="./mypage.php">마이페이지</a>
-		<?php }?>
+		<a class="mypage" href="./mypage.php">마이페이지</a>
+		<?php 
+		//session에 등록된 id 불러오기
+		$session_id=$_SESSION['ss_id'];
 		
-	<div class=maintitle align="center">
-		<a class="title" href='./mainview.php'>Houswap</a> 
-	</div>
-	
-			<h3 align="center">마이페이지</h3>
-			<hr>
-			
-			<h3 class="mypagetext">내정보</h3>
+		$sql = "SELECT * FROM customer WHERE c_id='$session_id';";
+		$result = mysqli_query($conn, $sql);
+		$assoc = mysqli_fetch_assoc($result);
+		?>
 		
+    <div class="maintitle" align="center">
+    		<a class="title" href='./mainview.php'>Houswap</a> 
+    		
+    		<h3 class=mypagetitle>마이페이지</h3>
+    <hr>
+     </div>
+    		<h3 class=myinfo>내 정보</h3>		
+    		
+    		<table class="mypage">
+    		<tr>
+    		<th class="mypage">아이디</th>
+    		<td class="mypage"><?php echo $assoc['c_id']?></td>
+          </tr>
+          <tr>
+             <th class="mypage">비밀번호</th>
+             <td class="mypage"><?php echo $assoc['c_password']?></td>
+          </tr>
+          <tr>
+             <th class="mypage">이름</th>
+             <td class="mypage"><?php echo $assoc['c_name']?></td>
+          </tr>
+          <tr>
+             <th class="mypage">성별</th>
+             <td class="mypage"><?php echo $assoc['c_gender']?></td>
+          </tr>
+          <tr>
+             <th class="mypage">생년월일</th>
+             <td class="mypage"><?php echo $assoc['c_birth']?></td>
+          </tr>
+          <tr>
+             <th class="mypage">이메일</th>
+             <td class="mypage"><?php echo $assoc['c_email']?></td>
+          </tr>
+          <tr>
+             <th class="mypage">휴대전화</th>
+             <td class="mypage"><?php echo $assoc['c_phonenum']?></td>
+          </tr>
+          <tr>
+             <th class="mypage">주소</th>
+             <td class="mypage"><?php echo $assoc['c_adress']?></td>
+          </tr>
+          
+       	  </table> <?php }?>
+
 </div>
 	
 </body>
