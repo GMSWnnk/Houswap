610cb1f0f977c51f69126ca7675c9dd81e6c47d3
jswlt <jswlt@LAPTOP-II2HCG5R>
2020-03-10 19:15:45 +0900

    mypage

--- Houswap/mypage.php
+++ Houswap/mypage.php
@@ -27,50 +27,76 @@
 		$result = mysqli_query($conn, $sql);
 		$assoc = mysqli_fetch_assoc($result);
 		?>
-		
+</div>		
     <div class="maintitle" align="center">
     		<a class="title" href='./mainview.php'>Houswap</a> 
     		
     		<h3 class=mypagetitle>마이페이지</h3>
     <hr>
      </div>
-    		<h3 class=myinfo>내 정보</h3>		
+     
+     	<div class=mypagetable>
+    		<h3 class=myinfo>회원 정보 수정</h3>		
     		
-    		<table class="mypage">
-    		<tr>
+    	<table class="mypage">
+    	<tr>
     		<th class="mypage">아이디</th>
-    		<td class="mypage"><?php echo $assoc['c_id']?></td>
-          </tr>
-          <tr>
+    		<td class="mypage">
+    		<input type="text" value="<?php echo $assoc['c_id']?>" readonly>
+    		</td>
+        </tr>
+        <tr>
              <th class="mypage">비밀번호</th>
-             <td class="mypage"><?php echo $assoc['c_password']?></td>
-          </tr>
-          <tr>
+             <td class="mypage">
+             <input type="text" value="<?php echo $assoc['c_password']?>" name="c_password">
+             </td>
+        </tr>
+        <tr>
              <th class="mypage">이름</th>
-             <td class="mypage"><?php echo $assoc['c_name']?></td>
-          </tr>
-          <tr>
+             <td class="mypage">
+             <input type="text" value="<?php echo $assoc['c_name']?>" name="c_name">
+             </td>
+        </tr>
+        <tr>
              <th class="mypage">성별</th>
-             <td class="mypage"><?php echo $assoc['c_gender']?></td>
-          </tr>
-          <tr>
+             <td class="mypage">
+             <select name="c_gender">
+             <option value="<?php echo $assoc['c_gender']?>"><?php echo $assoc['c_gender']?></option>
+             <?php
+             if($assoc['c_gender']=="여자"){?> 
+             <option value="남자">남자</option>
+             <?php }
+             else {?>
+			 <option value="여자">여자</option>  <?php }?>            
+             </select>
+             </td>
+        </tr>
+        <tr>
              <th class="mypage">생년월일</th>
-             <td class="mypage"><?php echo $assoc['c_birth']?></td>
-          </tr>
-          <tr>
+             <td class="mypage">
+             <input type="text" value="<?php echo $assoc['c_birth']?>" name="c_birth">
+             </td>
+        </tr>
+        <tr>
              <th class="mypage">이메일</th>
-             <td class="mypage"><?php echo $assoc['c_email']?></td>
-          </tr>
-          <tr>
+             <td class="mypage">
+             <input type="text" value="<?php echo $assoc['c_email']?>" name="c_email">
+             </td>
+        </tr>
+        <tr>
              <th class="mypage">휴대전화</th>
-             <td class="mypage"><?php echo $assoc['c_phonenum']?></td>
-          </tr>
-          <tr>
+             <td class="mypage">
+             <input type="text" value="<?php echo $assoc['c_phonenum']?>" name="c_phone">
+             </td>
+        </tr>
+        <tr>
              <th class="mypage">주소</th>
-             <td class="mypage"><?php echo $assoc['c_adress']?></td>
-          </tr>
+             <td class="mypage">
+             <input type="text" value="<?php echo $assoc['c_adress']?>" name="c_adress">
+             </td>
+        </tr>
           
-       	  </table> <?php }?>
+       	</table> <?php }?>
 
 </div>
 	
